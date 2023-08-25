b27436ff197ecd976e56fa7130fa638f60e2f0dd
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-08-25 21:24:39 +0700

    CRUD Category, Product, Auth Login, Authorization with Policy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,25 +2,28 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(5)->create();
 
+        // Uncomment the following lines if you want to create a test user
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(
+        $this->call([
             UserSeeder::class,
-        );
+            ProductSeeder::class,
+        ]);
     }
 }
