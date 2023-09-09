f20a331e4f9e6df7e0e04c19b631fbe94dbcfeff
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-09-09 13:24:48 +0700

    edit resource category api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:sanctum');
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
         $this->authorizeResource(Category::class,'category');
     }
     /**
