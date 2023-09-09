1676ee33f159f759487f6d4b0755e238831020fd
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2023-09-09 12:18:19 +0700

    edit resource category api

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -14,10 +14,6 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'description' => $this->description,
-        ];
+        return parent::toArray($request);
     }
 }
